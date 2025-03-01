17e39bcafb5d741fecda30b088fddfda964c4368
Arnold De La Vega <arnolddlv@icloud.com>
2025-02-28 23:22:59 -0800

    Changed Logo and added footer link to my website

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
       <body
         className={clsx(
           "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable,
+          fontSans.variable
         )}
       >
         <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
@@ -51,11 +51,10 @@ export default function RootLayout({
               <Link
                 isExternal
                 className="flex items-center gap-1 text-current"
-                href="https://heroui.com?utm_source=next-app-template"
-                title="heroui.com homepage"
+                href="https://arnolddelavega.com"
               >
-                <span className="text-default-600">Powered by</span>
-                <p className="text-primary">HeroUI</p>
+                <span className="text-default-600">Made by</span>
+                <p className="text-primary">Arnold</p>
               </Link>
             </footer>
           </div>
